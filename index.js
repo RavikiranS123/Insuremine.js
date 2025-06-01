a91e81702cf919acca0f0ae2b26332266580f002
Ravikiran S <sgrks123@gmail.com>
2025-06-01 17:34:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('This is for Insurmedmine'); //write a response to the client
+  res.write('Insuredmine 3rd floor, Gangothri Tower, L140, 5th Main Rd, above Jana Small Finance Bank, Sector 6, HSR Layout, Bengaluru, Karnataka 560034'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
